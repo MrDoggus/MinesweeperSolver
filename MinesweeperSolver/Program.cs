8d7b4924d2486ee539c02d3ec56f0987d4326561
MrDoggus <you@example.com>
2022-11-11 17:15:09 -0500

    Add project files.

---
+++ MinesweeperSolver/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+struct Coords
+{
+    int x;
+    int y;
+}
+
+class Square
+{
+    bool IsHidden;
+    bool IsMarked;
+    bool IsBomb;
+    byte AdjacentBombs;
+}
+
+class Board
+{
+    Coords Size;
+    double Bomb_Percent;
+}
